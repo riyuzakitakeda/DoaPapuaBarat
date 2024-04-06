26661d9803a5b883d65a5821b5344c1533b9eaf8
riyuzakitakeda <rpgfloor@gmail.com>
2024-04-06 23:02:28 +0800

    update databases

--- BE/config/db.config.js
+++ BE/config/db.config.js
@@ -1,8 +1,22 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "doa_papua",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "doa_papua",
+  USER: "doax3147_user",
+  PASSWORD: "Makassar@2024",
+  DB: "doax3147_db",
   dialect: "mysql",
   pool: {
     max: 5,
